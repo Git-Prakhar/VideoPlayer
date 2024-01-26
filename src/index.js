443e9e1433036adc882c4af88fb310e5b8a00107
Git-Prakhar <kprakhar01@gmail.com>
2024-01-26 15:36:50 +0530

    utils update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import app from './app.js'
 
 dotenv.config(
     {
@@ -7,4 +8,12 @@ dotenv.config(
     }
 )
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Listening on PORT : ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("ERROR : ",err);
+})
